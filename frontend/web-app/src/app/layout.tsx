94a174b8d75b8ce55f303b508b67a2e9010480a2
haidang <haiddhe171544@fpt.edu.vn>
2026-02-22 22:39:40 +0700

    fix css

--- frontend/web-app/src/app/layout.tsx
+++ frontend/web-app/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
 import QueryProvider from "@/providers/QueryProvider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "AccArenas",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <QueryProvider>
           <AuthProvider>{children}</AuthProvider>
         </QueryProvider>
